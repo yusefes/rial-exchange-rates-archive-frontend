c0c9474b65e2a39549aa81f8c69d75edb0a3186b
SamadiPour <samadipoor2@gmail.com>
2024-12-08 00:41:05 +0330

    feat: migrate from chartjs to ApexCharts

--- src/components/ExchangeRateChart.vue
+++ src/components/ExchangeRateChart.vue
@@ -1,28 +1,8 @@
 <script setup lang="ts">
 import {computed} from 'vue';
-import {Line} from 'vue-chartjs';
-import {
-  CategoryScale,
-  Chart as ChartJS,
-  Legend,
-  LinearScale,
-  LineElement,
-  PointElement,
-  Title,
-  Tooltip
-} from 'chart.js';
+import VueApexCharts from 'vue3-apexcharts';
 import {DateRange, ExchangeRatesData} from '../types/exchange';
 import {CURRENCIES} from '../constants/currencies';
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
 
 const props = defineProps<{
   data: ExchangeRatesData;
@@ -32,65 +12,83 @@ const props = defineProps<{
 
 const chartData = computed(() => {
   const dates = Object.keys(props.data)
-    .filter(date => {
-      const currentDate = new Date(date);
-      return currentDate >= props.dateRange.start && currentDate <= props.dateRange.end;
-    })
-    .sort();
+      .filter(date => {
+        const currentDate = new Date(date);
+        return currentDate >= props.dateRange.start && currentDate <= props.dateRange.end;
+      })
+      .sort();
 
-  return {
-    labels: dates,
-    datasets: props.selectedCurrencies.map(currency => {
-      const currencyData = CURRENCIES[currency];
-      // background color with alpha
-      const alpha = 0.5;
-      const hex = currencyData.color.substring(1);
-      const rgb = {
-        r: parseInt(hex.substring(0, 2), 16),
-        g: parseInt(hex.substring(2, 4), 16),
-        b: parseInt(hex.substring(4, 6), 16),
-      };
-      const backgroundColor = `rgba(${rgb.r}, ${rgb.g}, ${rgb.b}, ${alpha})`;
-      return {
-        label: `${currencyData?.flag || ''} ${currency.toUpperCase()}`,
-        data: dates.map(date => props.data[date]?.[currency]?.sell || null),
-        backgroundColor: backgroundColor,
-        borderColor: currencyData.color,
-        fill: true,
-        tension: 0,
-        borderWidth: 2.3,
-      };
-    }),
-  };
+  return props.selectedCurrencies.map(currency => {
+    const currencyData = CURRENCIES[currency];
+    return {
+      name: `${currencyData?.flag || ''} ${currency.toUpperCase()}`,
+      data: dates.map(date => props.data[date]?.[currency]?.sell || null),
+    };
+  });
 });
 
-const chartOptions = {
-  responsive: true,
-  maintainAspectRatio: false,
-  plugins: {
-    legend: {
-      position: 'top' as const,
+const chartOptions = computed(() => ({
+  chart: {
+    type: 'line',
+    zoom: {
+      enabled: false
     },
-  },
-  scales: {
-    y: {
-      beginAtZero: false,
+    animation: {
+      // disable if date range bigger than 2 years
+      enabled: (props.dateRange.end.getTime() - props.dateRange.start.getTime()) / (1000 * 60 * 60 * 24 * 365) <= 2,
     }
   },
-  interaction: {
-    mode: 'nearest',
-    axis: 'x',
-    intersect: false,
+  xaxis: {
+    type: 'datetime',
+    categories: Object.keys(props.data)
+        .filter(date => {
+          const currentDate = new Date(date);
+          return currentDate >= props.dateRange.start && currentDate <= props.dateRange.end;
+        })
+        .sort(),
   },
-};
+  yaxis: {
+    labels: {
+      formatter: (value: number) => value.toLocaleString('en-US'),
+    },
+    title: {
+      text: 'Price'
+    },
+    tickAmount: 10,
+  },
+  stroke: {
+    curve: 'straight',
+    width: 3,
+  },
+  fill: {
+    type: 'solid',
+    opacity: 0.5,
+  },
+  markers: {
+    // only if less than 31 days
+    size: (props.dateRange.end.getTime() - props.dateRange.start.getTime()) / (1000 * 60 * 60 * 24) <= 31 ? 4 : 0,
+  },
+  tooltip: {
+    shared: true,
+    intersect: false,
+    x: {
+      format: 'dd MMM yyyy',
+    },
+  },
+  legend: {
+    position: 'top',
+  },
+}));
 </script>
 
 <template>
   <div class="h-[600px]">
-    <Line
-      v-if="chartData.datasets.length > 0"
-      :data="chartData"
-      :options="chartOptions"
+    <VueApexCharts
+        v-if="chartData.length > 0"
+        type="line"
+        :options="chartOptions"
+        :series="chartData"
+        height="600"
     />
     <div v-else class="flex items-center justify-center h-full">
       Please select at least one currency to display the chart
