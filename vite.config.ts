a8e44a5a629c7a4cfce48cff25544850cba9b93c
SamadiPour <samadipoor2@gmail.com>
2024-12-08 01:39:37 +0330

    chore: change to make it ready for github page

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: process.env.NODE_ENV === 'production'
+      ? env.VITE_REPO_NAME || '/'
+      : '/',
+    plugins: [vue()],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
           }
         }
-      }
+      },
     },
-  },
+  }
 })
